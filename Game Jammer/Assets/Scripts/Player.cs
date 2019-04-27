be72a6432eaa065394af2534382fc9e30456d6c4
Gus <grizendi@gmail.com>
2019-04-26 19:07:10 -0700

    Update Player.cs

--- Game Jammer/Assets/Scripts/Player.cs
+++ Game Jammer/Assets/Scripts/Player.cs
@@ -2,20 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 [RequireComponent(typeof(Rigidbody))]
 
 public class Player : MonoBehaviour
 {
-    
+    [SerializeField] private int _playerNumber = 1;
+
     private Rigidbody _rigidBody;
+    private float _horizontalInput = 0f;
+    private float _verticalInput = 0f;
+
 
     private void Start()
     {
-        _rigidBody = GetComponent<Rigidbody>();    
+        _rigidBody = GetComponent<Rigidbody>();
+    
     }
 
 
+    private void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal_P" + _playerNumber);
+        _verticalInput = Input.GetAxis("Vertical_P" + _playerNumber);
+
+    }
+
+    private void FixedUpdate()
+    {
+   
+    }
 
 
 
