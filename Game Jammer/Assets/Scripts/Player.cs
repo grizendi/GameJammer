4ae5e76416696580b84320f1c79be0fb7a0b12d7
Gus <grizendi@gmail.com>
2019-04-26 18:16:19 -0700

    Player Input Added
    Added inputs for two players with different joysticks each

---
+++ Game Jammer/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Player : MonoBehaviour
+{
+    
+    private Rigidbody _rigidBody;
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();    
+    }
+
+
+
+
+
+
+}
