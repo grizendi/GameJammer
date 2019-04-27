0cefdb842df3ffece5562204a6906c5bba5f951c
Gus <grizendi@gmail.com>
2019-04-26 20:08:15 -0700

    Basic player movement working

--- Game Jammer/Assets/Scripts/Player.cs
+++ Game Jammer/Assets/Scripts/Player.cs
@@ -7,12 +7,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Tooltip("This ship represents player number...")]
     [SerializeField] private int _playerNumber = 1;
+
+    [Tooltip("Speed of the plane")]
     [SerializeField] private float _forwardSpeed = 10f;
+
+    [SerializeField] private float _rotateSpeed = 10f;
 
     private Rigidbody _rigidBody;
     private float _horizontalInput = 0f;
     private float _verticalInput = 0f;
+    private bool _fireInput = false;
+
 
     private void Start()
     {
@@ -25,12 +32,20 @@ public class Player : MonoBehaviour
     {
         _horizontalInput = Input.GetAxis("Horizontal_P" + _playerNumber);
         _verticalInput = Input.GetAxis("Vertical_P" + _playerNumber);
+        _fireInput = Input.GetButtonDown("Fire1_P" + _playerNumber);
 
     }
 
     private void FixedUpdate()
     {
         ThrustForward();
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        //Vector3 direction = new Vector3(0f, _horizontalInput, 0f);
+        transform.Rotate(0f, _horizontalInput * _rotateSpeed, 0f);
     }
 
     private void ThrustForward()
